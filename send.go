0536186bc67c71413ffe534dbe66c7253db506c5
xtony77 <xtony77@gmail.com>
2022-05-10 15:55:49 +0800

    set message delay time to consume

--- send.go
+++ send.go
@@ -24,10 +24,12 @@ func main() {
 		panic(err)
 	}
 
-	result, err := rocketmqProducer.SendSync(context.Background(), &primitive.Message{
+	msg := &primitive.Message{
 		Topic: topic,
 		Body:  []byte("Hello RocketMQ Go Client!"),
-	})
+	}
+	msg.WithDelayTimeLevel(0)
+	result, err := rocketmqProducer.SendSync(context.Background(), msg)
 	if err != nil {
 		panic(err)
 	}
