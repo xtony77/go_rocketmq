53544c49abc3b210fc8dbeee3c5a933a5c79a498
xtony77 <xtony77@gmail.com>
2022-05-10 17:29:12 +0800

    format send result

--- send.go
+++ send.go
@@ -6,7 +6,13 @@ import (
 	"github.com/apache/rocketmq-client-go/v2/primitive"
 	"context"
 	"fmt"
+	"encoding/json"
 )
+
+type MessageInfo struct {
+	Body   string
+	Result *primitive.SendResult
+}
 
 func main() {
 	topic := "testTopic"
@@ -24,14 +30,23 @@ func main() {
 		panic(err)
 	}
 
+	msgBody := "Hello RocketMQ Go Client!"
 	msg := &primitive.Message{
 		Topic: topic,
-		Body:  []byte("Hello RocketMQ Go Client!"),
+		Body:  []byte(msgBody),
 	}
 	msg.WithDelayTimeLevel(0)
+	// msg.WithTag("")
+	// msg.WithKeys([]string{""})
+
 	result, err := rocketmqProducer.SendSync(context.Background(), msg)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(result)
+
+	b, _ := json.Marshal(MessageInfo{
+		Body:   msgBody,
+		Result: result,
+	})
+	fmt.Println(string(b))
 }
